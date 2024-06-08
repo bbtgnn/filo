d249b1ebacda766eed00cad00a686926c062f0a9
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-06-08 03:07:08 +0200

    feat: navigation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Documents</h1>
-<a href="/documents/test_document">test_document</a>
+<div class="prose p-8">
+	<h1>Filo</h1>
+	<hr />
+	<h1>Prototypes</h1>
+	<ul>
+		<li>
+			<a href="/prototypes/load_document_move_cursor/[document_id]">test_document</a>
+		</li>
+		<li>
+			<a href="/prototypes/positioning">positioning</a>
+		</li>
+	</ul>
+</div>
---
+++ src/routes/prototypes/+layout.svelte
@@ -0,0 +1,7 @@
+<div class=" bg-stone-300 px-8 py-4">
+	<div class="prose">
+		<a href="/">{'<- Home'}</a>
+	</div>
+</div>
+
+<slot />
