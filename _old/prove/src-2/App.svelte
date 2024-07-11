c598e15e660ad5be75f7065f03fcc709bae7dd5f
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-07-11 17:13:27 +0200

    cleanup

--- _old/prove/src-2/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import Content from "./components/Content.svelte";
-  import { nodes } from "./stores/nodes";
-</script>
-
-<Content node={$nodes.node1} />
-<pre>{JSON.stringify($nodes, null, 4)}</pre>
--- _old/prove/src-4/App.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import Zone from "./components/Zone.svelte";
-  import { blocks } from "./stores/blocks";
-</script>
-
-<Zone bind:items={$blocks} />
-<pre>{JSON.stringify($blocks, null, 4)}</pre>
