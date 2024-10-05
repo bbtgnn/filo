c8aefc8f5ec576af8372374f5512d322c9cb94d7
Giovanni Abbatepaolo <30571828+bbtgnn@users.noreply.github.com>
2024-10-05 12:55:13 +0200

    feat: setup ctrl z

--- src/app/filo/filoControls.svelte
+++ src/app/filo/filoControls.svelte
@@ -83,6 +83,13 @@ SPDX-License-Identifier: AGPL-3.0-or-later
 			})
 		},
 		{
+			key: 'z',
+			modifier: ['ctrl', 'meta'],
+			callback: preventDefault(() => {
+				manager.undo();
+			})
+		},
+		{
 			key: ' ',
 			callback: preventDefault(() => {})
 		},
